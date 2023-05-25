28e7f8bb8492025708fa09029fb16f63d5c25f8a
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-05-26 02:04:09 +0300

    up Custom Min Function task using Collections.min

---
+++ javaProblemFunctionalProgramming/Exercise/task3_2CustomMinFunction.java
@@ -0,0 +1,19 @@
+package Exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class task3_2CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println(Collections.min(numbers));
+    }
+}
