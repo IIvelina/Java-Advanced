25c1cee179b3b4bb558db6031112027aa2a6bf10
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-05-26 02:13:29 +0300

    up Custom Min Function using list with Function

---
+++ javaProblemFunctionalProgramming/Exercise/task3_4CustomMinFunction.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class task3_4CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> getMinNumber = list -> Collections.min(list);
+
+        System.out.println(getMinNumber.apply(numbers));
+    }
+}
