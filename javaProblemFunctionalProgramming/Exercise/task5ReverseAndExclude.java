eacc14a05e595a156d9edda525d3706ee809f817
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-05-26 02:46:14 +0300

    up Reverse And Exclude task

---
+++ javaProblemFunctionalProgramming/Exercise/task5ReverseAndExclude.java
@@ -0,0 +1,27 @@
+package Exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class task5ReverseAndExclude {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(numbers);
+
+        Predicate<Integer> isDivisible = num -> num % n != 0;
+
+        numbers.stream().filter(isDivisible)
+                .forEach(digit -> System.out.print(digit + " "));
+    }
+}
