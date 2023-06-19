973fad919fcd30176126a2f98c6d339c903d169b
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-06-19 15:30:12 +0300

    up Comparable Book task

---
+++ javaProblemIteratorsAndComparators/Lab/ComparableBook/ComparableBook/Book.java
@@ -0,0 +1,61 @@
+package ComparableBook;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book implements Comparable<Book> {
+    private String title;
+    private int year;
+
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = new ArrayList<>(Arrays.asList(authors));
+        //new ArrayList<>(Arrays.asList(authors)) - превръща масив в списък
+    }
+
+
+    /*
+    public Book(String title, int year, List<String> authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = authors;
+    }
+    */
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    private void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public int compareTo(Book otherBook) {
+        if (this.getTitle().compareTo(otherBook.getTitle()) == 0){
+            //the book titles are equals
+            return Integer.compare(this.getYear(), otherBook.getYear());
+        }
+        return this.getTitle().compareTo(otherBook.getTitle());
+    }
+}
