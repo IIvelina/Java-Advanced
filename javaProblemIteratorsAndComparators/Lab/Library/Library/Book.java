ea8933255198aa5b8a4b8023d0bf72d82441770a
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2023-06-19 14:33:50 +0300

    up library task

---
+++ javaProblemIteratorsAndComparators/Lab/Library/Library/Book.java
@@ -0,0 +1,52 @@
+package Library;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = new ArrayList<>(Arrays.asList(authors));
+        //new ArrayList<>(Arrays.asList(authors)) - превръща масив в списък
+    }
+
+
+    /*
+    public Book(String title, int year, List<String> authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = authors;
+    }
+    */
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    private void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+}
